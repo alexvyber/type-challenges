755ce140573347c15e05c4971d8ec61266af4cfc
Alexey Sokolov <alexvyber@gmail.com>
2023-03-28 11:34:27 +0300

    first of array

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,10 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [infer A, ...any[]] ? A : never
+
+type Wrong<T extends any[]> = {
+  [key in T[number]]: key
+}
+
+type S = First<[1, 2, 3]>
+//   ^?
+type S2 = Wrong<[1, 2, 3]>
+//   ^?
