c1c19e0f1dffd63afd4e631dc891165e64bb7559
Alexey Sokolov <alexvyber@gmail.com>
2023-03-28 11:34:03 +0300

    tuple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,4 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly PropertyKey[]> = {
+  [key in T[number]]: key
+}
+type Example = TupleToObject<[1, 2, 3, 4]>
