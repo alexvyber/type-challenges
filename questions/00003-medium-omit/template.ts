9191e1daa29dfc937e271f7438bbd3732f9ba112
Alexey Sokolov <alexvyber@gmail.com>
2023-04-06 20:42:27 +0300

    00003-medium-omit

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,11 @@
-type MyOmit<T, K> = any
+type MyOmit<T extends object, K extends keyof T> = {
+  [Key in keyof T as Key extends K ? never : Key]: T[Key]
+}
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type R = MyOmit<Todo, "description">
