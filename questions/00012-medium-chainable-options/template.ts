69ab49bec0c64c75fd31f0ba409ee89f6f91be67
Alexey Sokolov <alexvyber@gmail.com>
2023-04-09 22:47:52 +0300

    00012-medium-chainable-options

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,52 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<T extends Record<string, any> = {}> = {
+  option<K extends string, V>(
+    key: K extends keyof T ? never : K,
+    value: V
+  ): Chainable<
+    { [Key in keyof T as K extends Key ? never : Key]: T[Key] } & {
+      [Key in K]: V
+    }
+  >
+  get(): T
+}
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+type Chainable2<T extends Record<string, any> = {}> = {
+  option<K extends string, V>(
+    key: K extends keyof T ? never : K,
+    value: V
+  ): Chainable2<
+    { [Key in keyof T as K extends Key ? never : Key]: T[Key] } & {
+      [Key in K]: V
+    }
+  >
+  get(): T
 }
