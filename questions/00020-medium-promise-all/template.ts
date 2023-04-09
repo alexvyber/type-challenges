4f33d019c0b497a5cff9b8a887e1b83dc74474aa
Alexey Sokolov <alexvyber@gmail.com>
2023-04-10 00:24:14 +0300

    00020-medium-promise-all

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,18 @@
-declare function PromiseAll(values: any): any
+declare function PromiseAll<T extends Array<any>>(
+  values: readonly [...T]
+): Promise<
+  T extends [any, ...any]
+    ? SomePromiseAll<T>
+    : T extends Array<infer A | Promise<infer A>>
+    ? A[]
+    : never
+>
+
+type SomePromiseAll<T, Acc extends Array<any> = []> = T extends [
+  infer Head,
+  ...infer Tail
+]
+  ? Head extends Promise<any>
+    ? SomePromiseAll<Tail, [...Acc, Awaited<Head>]>
+    : SomePromiseAll<Tail, [...Acc, Head]>
+  : Acc
