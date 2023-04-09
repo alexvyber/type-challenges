994c765d23eac407ee97ceb01bda28f98bfb8274
Alexey Sokolov <alexvyber@gmail.com>
2023-04-10 00:37:39 +0300

    00020-medium-promise-all - update

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,18 +1,31 @@
+// declare function PromiseAllTwo<T extends Array<any>>(
+//   values: readonly [...T]
+// ): Promise<
+//   T extends [any, ...any]
+//     ? SomePromiseAll<T>
+//     : T extends Array<infer A | Promise<infer A>>
+//     ? A[]
+//     : never
+// >
+
+// type SomePromiseAll<T, Acc extends Array<any> = []> = T extends [
+//   infer Head,
+//   ...infer Tail
+// ]
+//   ? Head extends Promise<any>
+//     ? SomePromiseAll<Tail, [...Acc, Awaited<Head>]>
+//     : SomePromiseAll<Tail, [...Acc, Head]>
+//   : Acc
+
+// const asdf = [1, 2, 3, 4] as const
+// type Some = typeof asdf
+// type Other = keyof Some
+
+// const other: Other = "1"
+
+// !!!!!!!!!!!!!!!!!!!!!
 declare function PromiseAll<T extends Array<any>>(
   values: readonly [...T]
-): Promise<
-  T extends [any, ...any]
-    ? SomePromiseAll<T>
-    : T extends Array<infer A | Promise<infer A>>
-    ? A[]
-    : never
->
-
-type SomePromiseAll<T, Acc extends Array<any> = []> = T extends [
-  infer Head,
-  ...infer Tail
-]
-  ? Head extends Promise<any>
-    ? SomePromiseAll<Tail, [...Acc, Awaited<Head>]>
-    : SomePromiseAll<Tail, [...Acc, Head]>
-  : Acc
+): Promise<{
+  [K in keyof T]: Awaited<T[K]>
+}>
