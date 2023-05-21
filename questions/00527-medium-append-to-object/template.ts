d23a010e2cbc0b8722995c0dba921f65bc3e8c9f
Alexey Sokolov <alexvyber@gmail.com>
2023-05-21 18:20:53 +0300

    00527-medium-append-to-object

--- questions/00527-medium-append-to-object/template.ts
+++ questions/00527-medium-append-to-object/template.ts
@@ -1,1 +1,16 @@
-type AppendToObject<T, U, V> = any
+type AppendToObject<
+  Obj extends object,
+  Prop extends PropertyKey,
+  Value
+> = Prettify<Obj & { [key in Prop]: Value }>;
+
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+type test1 = {
+  key: "cat";
+  value: "green";
+};
+
+type Some = AppendToObject<test1, "home", boolean>;
