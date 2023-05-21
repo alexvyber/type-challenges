d0ff1952f2267d9b8234e46c22946d807526bf10
Alexey Sokolov <alexvyber@gmail.com>
2023-05-21 19:17:24 +0300

    00599-medium-merge

--- questions/00599-medium-merge/template.ts
+++ questions/00599-medium-merge/template.ts
@@ -1,1 +1,24 @@
-type Merge<F, S> = any
+// type Merge<Left, Right> = Id<
+//   Pick<Left, Exclude<keyof Left, keyof Right>> &
+//     Pick<Right, Exclude<keyof Right, Optionals<Right>>> &
+//     Pick<Right, Exclude<Optionals<Right>, keyof Left>> &
+//     Props<Left, Right, Optionals<Right> & keyof Left>
+// >
+
+// type Id<T> = T extends infer U ? { [Key in keyof U]: U[Key] } : never
+
+// type Optionals<T> = {
+//   [Key in keyof T]-?: {} extends { [P in Key]: T[Key] } ? Key : never
+// }[keyof T]
+
+// type Props<Left, Right, Key extends keyof Left & keyof Right> = {
+//   [P in Key]: Left[P] | Exclude<Right[P], undefined>
+// }
+
+type Merge<Left, Right> = {
+  [Prop in keyof Left | keyof Right]: Prop extends keyof Right
+    ? Right[Prop]
+    : Prop extends keyof Left
+    ? Left[Prop]
+    : never;
+};
