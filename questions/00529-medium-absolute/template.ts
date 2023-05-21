914ab4ca88ce4292e36eef93093621622cffa83a
Alexey Sokolov <alexvyber@gmail.com>
2023-05-21 18:40:44 +0300

    00529-medium-absolute

--- questions/00529-medium-absolute/template.ts
+++ questions/00529-medium-absolute/template.ts
@@ -1,1 +1,6 @@
-type Absolute<T extends number | string | bigint> = any
+type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer U}`
+  ? U
+  : `${T}`;
+
+// type SomeRes = Absolute<18014398509481982n> // "18014398509481982"
+// type SomeRes2 = Absolute<-10>
