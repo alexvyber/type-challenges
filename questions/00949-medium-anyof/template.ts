c799acf009214b4249fd81897b9161ecaf49aa69
Alexey Sokolov <alexvyber@gmail.com>
2023-05-23 19:06:13 +0300

    update 00949-medium-anyof

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,10 +1,18 @@
 declare const emptyObjectSymbol: unique symbol;
-type EmptyObject = { [emptyObjectSymbol]?: never };
+// type EmptyObject = { [emptyObjectSymbol]?: never };
+// type EmptyObject = { [key in PropertyKey]: never };
+type EmptyObject = Record<PropertyKey, never>;
 
 type FalsyTypes = 0 | -0 | false | "" | null | undefined | EmptyObject | [];
 
+// type AnyOf<T extends readonly any[]> = (
+//   T extends Array<infer U> ? (U extends FalsyTypes ? false : true) : never
+// ) extends false
+//   ? false
+//   : true;
+
 type AnyOf<T extends readonly any[]> = (
-  T extends Array<infer U> ? (U extends FalsyTypes ? false : true) : never
+  T extends FalsyTypes[] ? false : true
 ) extends false
   ? false
   : true;
