8c7541bcb61952918e32805b76c370c0ea553660
Alexey Sokolov <alexvyber@gmail.com>
2023-05-23 18:46:47 +0300

    00949-medium-anyof

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,10 @@
-type AnyOf<T extends readonly any[]> = any
+declare const emptyObjectSymbol: unique symbol;
+type EmptyObject = { [emptyObjectSymbol]?: never };
+
+type FalsyTypes = 0 | -0 | false | "" | null | undefined | EmptyObject | [];
+
+type AnyOf<T extends readonly any[]> = (
+  T extends Array<infer U> ? (U extends FalsyTypes ? false : true) : never
+) extends false
+  ? false
+  : true;
