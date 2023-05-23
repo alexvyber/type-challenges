db5eada90968253f3fa886c1309bfe0c3dfddc19
Alexey Sokolov <alexvyber@gmail.com>
2023-05-23 17:21:41 +0300

    00645-medium-diff

--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,28 @@
-type Diff<O, O1> = any
+type Propertize<Prop, First, Second> = Prop extends keyof First
+  ? Prop extends keyof Second
+    ? never
+    : Prop
+  : Prop;
+
+type Diff<First, Second> = {
+  [Prop in keyof First | keyof Second as Propertize<
+    Prop,
+    First,
+    Second
+  >]: Prop extends keyof First
+    ? First[Prop]
+    : Prop extends keyof Second
+    ? Second[Prop]
+    : never;
+};
+
+type Foo = {
+  name: string;
+  age: string;
+};
+type Bar = {
+  name: string;
+  age: string;
+  gender: number;
+};
+
