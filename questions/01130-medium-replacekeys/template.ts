7641384a3a3396ad616a44a9269a9c434d88ca4a
Alexey Sokolov <alexvyber@gmail.com>
2023-05-24 22:25:16 +0300

    01130-medium-replacekeys

--- questions/01130-medium-replacekeys/template.ts
+++ questions/01130-medium-replacekeys/template.ts
@@ -1,1 +1,15 @@
-type ReplaceKeys<U, T, Y> = any
+type ReplaceKeys<Source, Keys extends keyof Replacement, Replacement> = {
+  [Prop in keyof Source]: Prop extends Keys & keyof Replacement
+    ? Prop extends keyof Replacement
+      ? Replacement[Prop]
+      : never
+    : Source[Prop];
+};
+
+// type ReplaceKeys<Source, Keys extends keyof Replacement, Replacement> = {
+//   [Prop in keyof Source]: Prop extends Keys & keyof Replacement
+//     // ? Prop extends keyof Replacement
+//       ? Replacement[Prop]
+//       // : never
+//     : Source[Prop];
+// }
