e2fb44114bcb36f88a9bebf3ec6b5093ba505051
Alexey Sokolov <alexvyber@gmail.com>
2023-05-24 14:56:45 +0300

    01097-medium-isunion

--- questions/01097-medium-isunion/template.ts
+++ questions/01097-medium-isunion/template.ts
@@ -1,1 +1,10 @@
-type IsUnion<T> = any
+// type IsUnion<T> = Exclude<T, never> extends never ? false : true
+// type IsUnion<T> = Exclude<T, T> extends never ? false : true
+
+type IsUnion<T, Copy = T> = [T] extends [never]
+  ? false
+  : T extends never
+  ? false
+  : [Copy] extends [T]
+  ? false
+  : true;
