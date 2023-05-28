407c6b5bdded3cfd376bae6e4d4a05d0c23ef4b9
Alexey Sokolov <alexvyber@gmail.com>
2023-05-28 19:15:31 +0300

    01978-medium-percentage-parser

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,9 @@
-type PercentageParser<A extends string> = any;
+type PercentageParser<A extends string> = [...ReturnSign<A>];
+
+type ReturnSign<T> = T extends `${infer First}${infer Rest}`
+  ? First extends "-" | "+"
+    ? [First, ...ReturnPercent<Rest>]
+    : ["", ...ReturnPercent<T>]
+  : ["", "", ""];
+  
+type ReturnPercent<T> = T extends `${infer U}${"%"}` ? [U, "%"] : [T, ""];
