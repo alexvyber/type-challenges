6f7ce7fbec3ec0e44782ae640289a6f0e95f93ba
Alexey Sokolov <alexvyber@gmail.com>
2023-05-28 18:18:08 +0300

    01367-medium-remove-index-signature

--- questions/01367-medium-remove-index-signature/template.ts
+++ questions/01367-medium-remove-index-signature/template.ts
@@ -1,1 +1,16 @@
-type RemoveIndexSignature<T> = any;
+type RemoveIndexSignature<T> = {
+  [Key in keyof T as string extends Key
+    ? never
+    : number extends Key
+    ? never
+    : symbol extends Key
+    ? never
+    : Key]: T[Key];
+};
+
+type Resssss = string extends "string" ? true : false;
+
+type ASFFFFFDSF = RemoveIndexSignature<{
+  [key: string]: any;
+  foo(): void;
+}>;
