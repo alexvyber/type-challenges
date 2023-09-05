15f2329eb9c71a165efdadbb387820830c956970
Alexey Sokolov <alexvyber@gmail.com>
2023-09-05 19:20:04 +0300

    02595-medium-pickbytype

--- questions/02595-medium-pickbytype/template.ts
+++ questions/02595-medium-pickbytype/template.ts
@@ -1,1 +1,13 @@
-type PickByType<T, U> = any;
+type PickByType<T extends object, U> = {
+  [Key in keyof T as T[Key] extends U ? Key : never]: T[Key]
+}
+
+
+interface Model {
+  name: string
+  count: number
+  isReadonly: boolean
+  isEnable: boolean
+}
+
+type O  =  PickByType<Model, boolean> 
