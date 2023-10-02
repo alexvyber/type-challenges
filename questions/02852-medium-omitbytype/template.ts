b40a70655a32a0be9113c619e77febfb06953395
Alexey Sokolov <alexvyber@gmail.com>
2023-10-02 18:44:31 +0300

    02852-medium-omitbytype

--- questions/02852-medium-omitbytype/template.ts
+++ questions/02852-medium-omitbytype/template.ts
@@ -1,1 +1,3 @@
-type OmitByType<T, U> = any;
+type OmitByType<T extends object, U  extends T[keyof T] > = {
+  [Key in keyof T as T[Key] extends U ? never : Key]: T[Key]
+};
--- questions/02852-medium-omitbytype/test-cases.ts
+++ questions/02852-medium-omitbytype/test-cases.ts
@@ -12,3 +12,7 @@ type cases = [
   Expect<Equal<OmitByType<Model, string>, { count: number; isReadonly: boolean; isEnable: boolean }>>,
   Expect<Equal<OmitByType<Model, number>, { name: string; isReadonly: boolean; isEnable: boolean }>>,
 ]
+
+type A = OmitByType<Model, string>
+type V = OmitByType<Model, symbol>
+// TODO: write test cases for constraints and may be PR it to main repo
