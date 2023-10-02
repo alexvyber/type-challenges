66c9053b9c96fbf4f71b6af83187f49b1a187984
Alexey Sokolov <alexvyber@gmail.com>
2023-10-02 19:13:30 +0300

    02946-medium-objectentries

--- questions/02946-medium-objectentries/template.ts
+++ questions/02946-medium-objectentries/template.ts
@@ -1,1 +1,13 @@
-type ObjectEntries<T> = any;
+// type ObjectEntries<T> = {
+//     [Key in keyof Required<T>]: [Key, Required<T>[Key] extends never ? undefined : Required<T>[Key]]
+//   }[keyof T]
+
+  type ObjectEntries<T> = {
+    [P in keyof Required<T>]: [P, Required<T>[P] extends never ? undefined : Required<T>[P] ]
+  }[keyof T]
+
+  // Op<Required<T>>
+// type Op<T> = {
+//   [Key in keyof T]: [Key, T[Key] extends never ? undefined : T[Key]]
+// }[keyof T]
+
