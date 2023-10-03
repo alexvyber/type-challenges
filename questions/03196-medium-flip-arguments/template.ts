1f8325747b9d232d2c03c997e81c61f9ee7b7856
Alexey Sokolov <alexvyber@gmail.com>
2023-10-03 19:08:03 +0300

    03196-medium-flip-arguments

--- questions/03196-medium-flip-arguments/template.ts
+++ questions/03196-medium-flip-arguments/template.ts
@@ -1,1 +1,17 @@
-type FlipArguments<T> = any;
+type FlipArguments<T extends (...args: any[]) => any> = ( ...args: Reverse<Parameters<T>>) => ReturnType<T>
+
+
+
+
+
+
+
+
+
+
+// type ResFlipArguments = FlipArguments<
+// 	(arg0: string, arg1: number, arg2: boolean) => void
+// >;
+// type ResFlipArguments2 = FlipArguments<(foo: string) => number>
+
+// type TupleRes = Parameters<(arg0: string, arg1: number, arg2: boolean) => void>
