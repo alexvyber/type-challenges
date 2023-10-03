adfdb3ba24aec3f0e1c7ae9e0d086b23ebdc2762
Alexey Sokolov <alexvyber@gmail.com>
2023-10-03 18:33:41 +0300

    03188-medium-tuple-to-nested-object

--- questions/03188-medium-tuple-to-nested-object/template.ts
+++ questions/03188-medium-tuple-to-nested-object/template.ts
@@ -1,1 +1,9 @@
-type TupleToNestedObject<T, U> = any;
+type TupleToNestedObject<T extends Array<PropertyKey>, U> = T extends [
+	infer First extends PropertyKey,
+	...infer Rest extends Array<PropertyKey>,
+]
+	? { [key in First]: TupleToNestedObject<Rest, U> }
+	: U;
+
+
+type ResTupleToNestedObject = TupleToNestedObject<["a", "b", "c"], boolean>;
