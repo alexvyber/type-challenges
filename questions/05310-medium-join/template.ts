ccef900ab78ea5ff53df494af131040c2f9e09af
Alex Vyber <alexvyber@gmail.com>
2023-10-15 11:43:10 +0300

    05310-medium-join

--- questions/05310-medium-join/template.ts
+++ questions/05310-medium-join/template.ts
@@ -1,1 +1,8 @@
-type Join<T, U> = any;
+type Join<T extends string[], U extends string | number> = T extends [
+  infer Letter extends string,
+  ...infer Rest extends string[]
+]
+  ? `${Letter}${Rest extends [] ? "" : U}${Join<Rest, U>}`
+  : ""
+// Rest extends never ? `${Letter}`  :
+type JoinRes = Join<["a", "p", "p", "l", "e"], "-">
