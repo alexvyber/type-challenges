47d0aa75f712070c02249db0066e73f35b46678a
Alex Vyber <alexvyber@gmail.com>
2023-10-15 11:43:21 +0300

    05821-medium-maptypes

--- questions/05821-medium-maptypes/template.ts
+++ questions/05821-medium-maptypes/template.ts
@@ -1,1 +1,10 @@
-type MapTypes<T, R> = any;
+type MapTypes<T extends object, R extends { mapFrom: any; mapTo: any }> = {
+  [K in keyof T]: T[K] extends R["mapFrom"]
+    ? R extends { mapFrom: T[K] }
+      ? R["mapTo"]
+      : never
+    : T[K]
+}
+
+type Arg = { mapFrom: string; mapTo: boolean } | { mapFrom: Date; mapTo: string }
+type ResMapTypes = MapTypes<{ name: string; date: Date }, Arg>
