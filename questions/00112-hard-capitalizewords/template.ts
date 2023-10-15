c7e8d01ec2659d9864aa40443d4b29aed51a0cb2
Alex Vyber <alexvyber@gmail.com>
2023-10-15 11:40:21 +0300

    00112-hard-capitalizewords

--- questions/00112-hard-capitalizewords/template.ts
+++ questions/00112-hard-capitalizewords/template.ts
@@ -1,1 +1,35 @@
-type CapitalizeWords<S extends string> = any
+// type CapitalizeWordsDel = "!@#$%^&*()_+{}|pp🤣qq"
+type CapitalizeWordsSymbols =
+  | "a"
+  | "b"
+  | "c"
+  | "d"
+  | "e"
+  | "f"
+  | "g"
+  | "h"
+  | "i"
+  | "j"
+  | "k"
+  | "l"
+  | "m"
+  | "n"
+  | "o"
+  | "p"
+  | "q"
+  | "r"
+  | "s"
+  | "t"
+  | "u"
+  | "v"
+  | "w"
+  | "x"
+  | "y"
+  | "z"
+
+type CapitalizeWords<S extends string> =
+  S extends `${infer First extends CapitalizeWordsSymbols}${infer Del}${infer Rest extends CapitalizeWordsSymbols}`
+    ? `${Capitalize<First>}${Del}${CapitalizeWords<Rest>}`
+    : Capitalize<S>
+
+type CapitalizeWordsRes = CapitalizeWords<"foo bar!hello world">
