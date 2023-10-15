21d98505a8aa66d6541427385c057ffd0dae27b3
Alex Vyber <alexvyber@gmail.com>
2023-10-15 11:43:45 +0300

    07544-medium-construct-tuple

--- questions/07544-medium-construct-tuple/template.ts
+++ questions/07544-medium-construct-tuple/template.ts
@@ -1,1 +1,5 @@
-type ConstructTuple<L extends number> = any;
+type ConstructTuple<L extends number> = ConstructTupleRec<[], L>
+type ConstructTupleRec<T extends any[], L> = T["length"] extends L
+  ? T
+  : ConstructTupleRec<[unknown, ...T], L>
+type ConstructTupleRes = ConstructTuple<2>
