130e1ccb29c1bd2d1ba654be786af8012f7a8024
Alex Vyber <alexvyber@gmail.com>
2023-10-15 11:42:38 +0300

    04484-medium-istuple

--- questions/04484-medium-istuple/template.ts
+++ questions/04484-medium-istuple/template.ts
@@ -1,1 +1,11 @@
-type IsTuple<T> = any;
+type IsTuple<T> = IsTupleCheck<T> extends never ? false : IsTupleCheck<T>
+
+type IsTupleCheck<T> = T extends [any]
+  ? true
+  : T extends readonly [any]
+  ? true
+  : T extends []
+  ? true
+  : false
+
+type ResIsTuple = IsTuple<never>
