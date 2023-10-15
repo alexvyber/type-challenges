c21f884666386458b0acdd4f4c8941f38620a4f4
Alex Vyber <alexvyber@gmail.com>
2023-10-15 11:42:18 +0300

    03326-medium-bem-style-string

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,7 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any;
+type BEM<B extends string, E extends string[], M extends string[]> = `${
+  B //`${B}${E[number] extends never ? "" : `__${E[number]}` }${M[number] extends never ? "" : `--${M[number]}`}`;
+}${WithPrefix<E, "__">}${WithPrefix<M, "--">}`
+
+type WithPrefix<Str extends string[], Prefix extends string> = Str[number] extends never
+  ? ""
+  : `${Prefix}${Str[number]}`
