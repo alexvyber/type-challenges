7929438c503b737d5a079e7d0c749e1a6e7ab016
Alex Vyber <alexvyber@gmail.com>
2023-10-15 11:43:52 +0300

    17973-medium-deepmutable

--- questions/17973-medium-deepmutable/template.ts
+++ questions/17973-medium-deepmutable/template.ts
@@ -1,1 +1,7 @@
-type DeepMutable = any
+type DeepMutable<T extends object> = {
+  -readonly [P in keyof T]: T[P] extends (...args: any[]) => any
+    ? T[P]
+    : T[P] extends object
+    ? DeepMutable<T[P]>
+    : T[P]
+}
